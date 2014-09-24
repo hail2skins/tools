dd33d69ad9cd03ae23e67351781222648114dfc4
Art Mills <art@hamcois.com>
2014-09-24 03:26:03 +0000

    Sign up implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
   root "projects#index"
 
   resources :projects do
     resources :tickets
   end
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
