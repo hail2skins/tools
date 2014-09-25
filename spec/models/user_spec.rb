b3d394ccd731c197ce20ba2e5be1117fabefc2c4
Art Mills <art@hamcois.com>
2014-09-25 03:37:23 +0000

    Listing 7.18.   Errors in users_controller with deleting blank passwords.   May skip this a while.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   describe "passwords" do
   	it "needs a password and confirmation to save" do
-  		u = User.new(name: "steve")
+  		u = User.new(name: "steve", email: "steve@example.com")
 
   		u.save
   		expect(u).to_not be_valid
@@ -26,6 +26,18 @@ RSpec.describe User, :type => :model do
 
   		expect(u).to_not be_valid
   	end
+
+    it "requires an email" do
+      u = User.new(name: "steve",
+                   password: "hunter2",
+                   password_confirmation: "hunter2")
+      u.save
+      expect(u).to_not be_valid
+
+      u.email = "steve@example.com"
+      u.save
+      expect(u).to be_valid
+    end
   end
 
   describe "authentication" do
