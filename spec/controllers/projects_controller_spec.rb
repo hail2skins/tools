60852676b6f1680c2057c4e504d65c288624e1be
Art Mills <art@hamcois.com>
2014-09-25 00:49:08 +0000

    Restrict access to project actions to admins only

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -9,13 +9,19 @@ RSpec.describe ProjectsController, :type => :controller do
 			sign_in(user)
 		end
 
-		it "cannot access the new action" do
-			get :new
+		{ new: :get,
+			create: :post,
+			edit: :get,
+		  update: :put,
+			destroy: :delete }.each do |action, method|
+		it "cannot access the #{action} action" do
+			send(method, action, :id => FactoryGirl.create(:project))
 
 			expect(response).to redirect_to('/')
 			expect(flash[:alert]).to eql("You must be an admin to do that.")
 		end
 	end
+end
 	
 	it "displays an error for a missing project" do
 		get :show, id: "not-here"
--- spec/features/deleting_projects_spec.rb
+++ spec/features/deleting_projects_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 feature "Deleting projects" do
+	before do
+		sign_in_as!(FactoryGirl.create(:admin_user))
+	end
+
 	scenario "Deleting a project" do
 		FactoryGirl.create(:project, name: "TextMate 2")
 
-		visit '/'
+		visit projects_path
 		click_link "TextMate 2"
 		click_link "Delete Project"
 
 		expect(page).to have_content("Project has been destroyed.")
 
-		visit '/'
+		visit projects_path
 
 		expect(page).to have_no_content("TextMate2")
 		end
