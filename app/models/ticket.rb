b84d7b8d6c0e8bff996d0bc7281e5d0c435b33ba
Art Mills <art@hamcois.com>
2014-10-03 21:19:25 +0000

    More on tags, but still broken.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,9 +5,12 @@ class Ticket < ActiveRecord::Base
 	has_many :assets
 	accepts_nested_attributes_for :assets
 	has_many :comments
+	attr_accessor :tag_names
+	has_and_belongs_to_many :tags
 
 	validates :title, presence: true
 	validates :description, presence: true,
 													length: { minimum: 10 }
+
 
 end
